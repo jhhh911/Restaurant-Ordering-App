fff92a402ebcf20f28a9301dfd216ec87a07fa48
James Heiniger <james.heiniger911@gmail.com>
2025-11-10 12:52:02 +1030

    Opening footer, adding orders to html and fixed css

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { menuArray } from "./data.js";
 
-function render() {
+const orderArray = [];
+
+function renderItems() {
   return menuArray
     .map(item => {
       const { name, ingredients, id, price, emoji } = item;
       return `
-    <section class="item-container" id='${name}-${id}'>
+    <section class="item-container">
       <div class='content'>
         <div class="emoji" aria-label='${name}'>${emoji}</div>
           <div class="text">
@@ -14,7 +16,7 @@ function render() {
             <p class="bold">$${price}</p>
           </div>
         </div>
-        <button class="order-btn">+</button>
+        <button class="order-btn" data-add='${id}'>+</button>
       </section>
       <div class='border-bottom'></div>
     `;
@@ -22,4 +24,35 @@ function render() {
     .join("");
 }
 
-document.getElementById("main").innerHTML = render();
+document.getElementById("main").innerHTML = renderItems();
+
+const footer = document.querySelector(".footer");
+const itemContainer = document.querySelector('.order-title')
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    openOrderSummary(e.target.dataset.add);
+    
+  }
+});
+
+function openOrderSummary(orderId) {
+  let order = ''
+  menuArray.forEach(item => {
+    if(item.id === Number(orderId)) {
+      orderArray.push(item)
+      footer.classList.add('footer-active')
+      order += `
+    <div class="item-content">
+        <div class="item">
+          <h3>${item.name}</h3>
+          <button class="remove-btn">remove</button>
+        </div>
+        <p class="bold price">$${item.price}</p>
+      </div>
+  `
+  itemContainer.innerHTML += order
+    } 
+  });
+}
+
