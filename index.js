8e8bd7a7a6e47c13ba1cd03c822ce02334bf7ade
James Heiniger <james.heiniger911@gmail.com>
2025-11-15 17:09:04 +1030

    Fixing and adding functions as well as css

--- index.js
+++ index.js
@@ -1,82 +1,107 @@
 import { menuArray } from "./data.js";
 
-const orderArray = [];
+const orderContainer = document.getElementById("order-container");
+const orderList = document.getElementById("order");
 
-const footer = document.querySelector(".footer");
-const itemContainer = document.querySelector(".order-container");
-const totalPrice = document.getElementById("price");
+let orderArray = [];
 
-document.addEventListener("click", function (e) {
-  if (e.target.dataset.add) {
-    openOrderSummary(e.target.dataset.add);
-  } else if (e.target.classList.contains("remove-btn")) {
-    handleRemove(e.target);
-  }
-});
-
-function renderItems() {
-  return menuArray
-    .map(item => {
-      const { name, ingredients, id, price, emoji } = item;
+// Render Menu
+const menuHTML = menuArray
+  .map(item => {
+    const { name, ingredients, id, price, emoji } = item;
       return `
-    <section class="item-container">
-      <div class='content'>
-        <div class="emoji" aria-label='${name}'>${emoji}</div>
-          <div class="text">
-            <h3 class='name'>${name}</h3>
-            <p class='ingredients'>${ingredients}</p>
-            <p class="bold">$${price}</p>
+      <div class="menu-item">
+        <div class='menu-content'>
+          <p class="emoji" aria-label='${name}'>${emoji}</p>
+          <div class="menu-text">
+            <p class='item-name'>${name}</p>
+            <p class='item-ingredients'>${ingredients}</p>
+            <p class="item-price">$${price}</p>
           </div>
         </div>
-        <button class="order-btn" data-add='${id}'>+</button>
-      </section>
-      <div class='border-bottom'></div>
+        <button class="add-btn" data-add='${id}'>+</button>
+      </div>
     `;
     })
     .join("");
-}
 
-document.getElementById("main").innerHTML = renderItems();
+document.getElementById("menu-container").innerHTML = menuHTML;
 
-const priceArray = [];
 
-function openOrderSummary(orderId) {
-  let order = "";
-  menuArray.forEach(item => {
-    if (item.id === Number(orderId)) {
-      orderArray.push(item);
-      priceArray.push(item.price);
-      if (!footer.classList.contains("footer-active")) {
-        footer.classList.add("footer-active");
-      }
-      order += `
-    <div class="item-content">
-    <div class="item">
-      <h3>${item.name}</h3>
-      <button class="remove-btn" data-remove="${item.id}">remove</button>
-    </div>
-    <p class="bold price">$${item.price}</p>
-  </div>
-  `;
+document.addEventListener("click", e => {
+  if (e.target.dataset.add) {
+    addItemToOrder(e.target.dataset.add);
+  } else if (e.target.dataset.delete) {
+    deleteItemFromOrder(e.target.dataset.delete);
+  }
+});
 
-      itemContainer.innerHTML += order
+// add item to order
+
+function addItemToOrder(itemId) {
+  const matchingItem = menuArray.find(item => item.id === Number(itemId))
+  const existingItem = orderArray.find(item => item.id === Number(itemId))
+    if (!existingItem) {
+      matchingItem.quantity = 1;
+      orderArray.push(matchingItem);
+      renderOrderItem(matchingItem);
+    } else {
+      existingItem.quantity++
+      updateOrderItem(matchingItem);
     }
-  });
-
-  let value = priceArray.reduce((acc, cur) => acc + cur, 0);
-  totalPrice.innerHTML = `$${value}`;
+    renderOrderData()
+    calculateOrderTotal()
 }
 
-function handleRemove(buttonEl) {
+//show/hide order container
+function renderOrderData() {
+  orderContainer.style.display = orderArray.length > 0 ? 'block' : 'none';
+}
+
+// render individual order item
+function renderOrderItem(item) {
+  orderList.innerHTML += `
+    <div class='order-items' id='order-${item.id}'>
+      <div class='order-items-text'>
+        <p class='order-item-title'>${item.name}</p>
+        <p class='order-item-quantity' id='order-quantity-${item.id}'>x ${item.quantity}</p>
+        <i class='fa-solid fa-trash' data-delete='${item.id}'></i>
+      </div>
+      <p class="order-item-price" id='order-price-${item.id}'>$${item.price}</p>
+    </div>
+  `;
+}
+
+// Update item quantity & price in DOM
+
+function updateOrderItem(item) {
+  document.getElementById(
+    `order-quantity-${item.id}`).innerHTML = `x ${item.quantity}`;
+  document.getElementById(`order-price-${item.id}`).innerHTML = `$${
+    item.price * item.quantity
+  }`;
+}
+
+// Delete item from order
+function deleteItemFromOrder(buttonEl) {
   // var to find the id of deleted item
   const orderId = Number(buttonEl.dataset.remove)
   // find the index of the order in the Order array
   const index = orderArray.findIndex(item => item.id === orderId)
   if (index !== -1) {
     orderArray.splice(index, 1)
-    priceArray.splice(index, 1)
   }
   if(orderArray.length === 0) {
     footer.classList.remove('footer-active')
   }
 }
+
+//show/hide order container
+
+// calculate total price
+function calculateOrderTotal() {
+  const total = orderArray.reduce((acc, cur) => acc + cur, 0);
+  totalPrice.innerHTML = `$${total}`;
+}
+
+// bring up the pay form & message
